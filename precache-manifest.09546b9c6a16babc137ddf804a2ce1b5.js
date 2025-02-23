45e5a60f4b24d14fa1649618450a5c8b6526090f
Josee <josee@example.com>
2025-02-23 15:27:33 -0600

    Updates

---
+++ precache-manifest.09546b9c6a16babc137ddf804a2ce1b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf00f2ee102a63e8301aa0ee1f9977fd",
+    "url": "/mi-proyecto/index.html"
+  },
+  {
+    "revision": "ca4718f4cdac85bb1d10",
+    "url": "/mi-proyecto/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "36af7ff4bdf87cd1bf53",
+    "url": "/mi-proyecto/static/js/2.a9def069.chunk.js"
+  },
+  {
+    "revision": "92da818bc82464fd0e53",
+    "url": "/mi-proyecto/static/js/3.70a4dfb6.chunk.js"
+  },
+  {
+    "revision": "ca4718f4cdac85bb1d10",
+    "url": "/mi-proyecto/static/js/main.5ca7f096.chunk.js"
+  },
+  {
+    "revision": "67947f5aa8b1b6f420c2",
+    "url": "/mi-proyecto/static/js/runtime~main.16e4e9eb.js"
+  }
+]);
